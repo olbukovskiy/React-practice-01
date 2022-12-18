3890b8d4988143745243a371ff6ff758cefd4bda
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-12-18 16:02:31 +0200

    task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,71 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+import PropTypes from 'prop-types';
 
-export const Statistics = () => {
+// const iconsArray = [
+//   FaRegThumbsUp,
+//   MdPeople,
+//   MdOutlineProductionQuantityLimits,
+//   GiTreeDoor,
+// ];
+
+// export const Statistics = ({ title, stats }) => {
+//   const newData = stats.reverse().map((stat, idx) => {
+//     return { ...stat, icon: iconsArray[idx] };
+//   });
+//   return (
+//     <>
+//       <StatisticTitle>{title}</StatisticTitle>
+//       <StatisticsList>
+//         {newData.map(stat => {
+//           return (
+//             <StatisticItem
+//               title={stat.title}
+//               total={stat.total}
+//               icon={stat.icon}
+//             />
+//           );
+//         })}
+//       </StatisticsList>
+//     </>
+//   );
+// };
+
+const iconsObject = {
+  1: FaRegThumbsUp,
+  2: MdPeople,
+  3: MdOutlineProductionQuantityLimits,
+  4: GiTreeDoor,
+};
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map(stat => {
+          return (
+            <StatisticItem
+              key={stat.id}
+              title={stat.title}
+              total={stat.total}
+              icon={iconsObject[stat.id]}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      total: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
