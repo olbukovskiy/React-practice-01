47e6a7fe58b3daf49ebc3bd35a86954dd6386f8b
mrsDiana <105992358+mrsDiana@users.noreply.github.com>
2022-12-18 15:16:27 +0200

    Task 01

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+
 import {
   Card,
   CardBody,
@@ -11,9 +13,36 @@ import {
   UserBox,
   UserInfo,
   UserName,
-  Date,
+  Date as DatePosted,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({poster, tag, title, description, userName, avatar, postedAt}) => {
+  const date= formatDistanceToNow(
+    new Date(postedAt));
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster
+          src={poster}
+          alt={tag}
+        />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>
+         {description}
+        </CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt="Jane Doe" />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <DatePosted>{date} ago</DatePosted>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
